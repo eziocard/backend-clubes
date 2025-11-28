1d48bbca8f586fc8f924bbf3515a19950763469a
eziocard <rjlopezjobs@gmail.com>
2025-11-27 23:44:28 -0300

    creacion de team seeder

---
+++ backend-clubes/database/seeders/TeamSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Team;
+use Illuminate\Support\Facades\Hash;
+
+class TeamSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Team::create([
+            'name' => 'tornado',
+            'email' => 'tornado@gmail.com',
+            'image' => null,
+            'password' => Hash::make('123456789'), 
+            'state' => true
+        ]);
+    }
+}
